38d83bf75d95cdd2689f18ad5d2769547f1eea10
Euan Greenwood <euan.greenwood@and.digital>
2021-11-19 13:24:13 +0000

    init repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,36 @@
-module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Gatsby Blog",
-  },
-  plugins: [
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+import 'dotenv';
+
+export const siteMetadata = {
+  siteUrl: 'https://www.yourdomain.tld',
+  title: 'Gatsby Blog',
 };
+export const plugins = [
+  'gatsby-plugin-styled-components',
+  'gatsby-plugin-image',
+  'gatsby-plugin-sharp',
+  'gatsby-transformer-sharp',
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      name: 'images',
+      path: './src/images/',
+    },
+    __key: 'images',
+  },
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `x4nvm4g34swg`,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  },
+  {
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: `x4nvm4g34swg`,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      host: `preview.contentful.com`,
+    },
+  },
+  'gatsby-plugin-gatsby-cloud',
+];
